cc816efcc60e96e62d62d462bd117d72d70ae670
leonardo_goandete <leonardo_goandete>
2023-09-28 12:16:42 -0300

    First commit

---
+++ src/main/java/br/com/ifrs/ExampleResource.java
@@ -0,0 +1,24 @@
+package br.com.ifrs;
+
+import io.smallrye.config.ConfigMapping;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@Path("/hello")
+public class ExampleResource {
+    @ConfigProperty(name = "ifrs.pw2")
+    private String mensagem;
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return this.getMensagem();
+    }
+}
